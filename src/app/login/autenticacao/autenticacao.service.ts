4ca89aa9e8c19ebc106814671a35e1655991b7f1
matheussc101 <matheussc101@gmail.com>
2017-11-25 01:43:11 -0200

    Deploy

--- src/app/login/autenticacao/autenticacao.service.ts
+++ src/app/login/autenticacao/autenticacao.service.ts
@@ -44,6 +44,8 @@ export class AutenticacaoService {
     let body = JSON.stringify(usuario);
     let headers = new Headers({ 'Content-Type': 'application/json' });
     let options = new RequestOptions({ headers: headers });
+    
+    console.log('entrou')
   
     this.http.post(`https://serverefesta.herokuapp.com/api/login/provisorio`, body, options).map((res: Response) => res.json())
     .subscribe(
